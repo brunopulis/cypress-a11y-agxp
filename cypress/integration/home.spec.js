1e67057905ef0de36510607f1df730cc4e0e423b
Bruno Pulis <bruno.pulis@gmail.com>
2020-12-14 02:08:50 -0300

    add new tests

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,32 +1,9 @@
-function terminalLog(violations) {
-  cy.task(
-    'log',
-    `${violations.length} accessibility violation${violations.length === 1 ? '' : 's'
-    } ${violations.length === 1 ? 'was' : 'were'} detected`
-  )
-  // pluck specific keys to keep the table readable
-  const violationData = violations.map(
-    ({ id, impact, description, nodes }) => ({
-      id,
-      impact,
-      description,
-      nodes: nodes.length
-    })
-  )
-
-  cy.task('table', violationData)
-}
-
 /// <reference types="cypress" />
 
 describe('Home', () => {
   beforeEach(() => {
     cy.visit('/')
     cy.injectAxe()
-  })
-
-  it('Has no detectable a11y violations on load', () => {
-    // cy.checkA11y() // fail for a11y violations
-    cy.checkA11y(null, null, terminalLog)
+    cy.checkA11y()
   })
 })
---
+++ cypress/integration/home_avancado.spec.js
@@ -0,0 +1,35 @@
+function terminalLog(violations) {
+  cy.task(
+    'log',
+    `${violations.length} accessibility violation${violations.length === 1 ? '' : 's'
+    } ${violations.length === 1 ? 'was' : 'were'} detected`
+  )
+  // pluck specific keys to keep the table readable
+  const violationData = violations.map(
+    ({ id, impact, description, nodes }) => ({
+      id,
+      impact,
+      description,
+      nodes: nodes.length
+    })
+  )
+
+  cy.task('table', violationData)
+}
+
+/// <reference types="cypress" />
+
+describe('Home', () => {
+  beforeEach(() => {
+    cy.visit('/')
+    cy.injectAxe()
+  })
+
+  it('Exibe resultado dos testes visualmente', () => {
+    cy.checkA11y()
+  })
+
+  it('Exibe resultado dos testes no terminal', () => {
+    cy.checkA11y(null, null, terminalLog)
+  })
+})
